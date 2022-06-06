d5eb63f5fa4aa3cdfc114d1280477400df27aaac
32dantey <72943266+32dantey@users.noreply.github.com>
2022-06-07 00:41:09 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,8 +10,8 @@
    networks: {
      hardhat: {},
      rinkeby: {
-         url: 'https://eth-rinkeby.alchemyapi.io/v2/OIFca-LooqN9071XE-D7gkZL0QwdwAbk',
-         accounts: [`379851c8072e5b1c1852e0b2fe6564d7ec843df2b82a281f4d91778356482c51`]
+         url: '',
+         accounts: [``]
      }
    }
  };
